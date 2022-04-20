c9765cda6e602b39e74f8d6856c37bcff83567fe
thaophung <thaoducphung@gmail.com>
2022-04-21 02:53:16 +0300

    Add fix scrolling screen when modal show

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home = ({
 
   if (loading) return null
   return (
-    <div className="relative h-screen bg-gradient-to-b ">
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${showModal && "!h-screen overflow-hidden"}`}>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
